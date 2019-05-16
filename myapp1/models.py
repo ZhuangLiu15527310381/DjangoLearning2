124beb56dae1541c6e3a0b75cf588b235a56bba6
ZhuangLiu15527310381 <1173893305@qq.com>
2019-05-16 21:48:44 +0800

    test1

--- myapp1/models.py
+++ myapp1/models.py
@@ -8,10 +8,9 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
-
-
 #test
 class Comment(models.Model):
     comment_dateTime = models.DateTimeField()
     comment_body = models.TextField()
-    blogPost = models.ForeignKey('BlogPost',on_delete=models.CASCADE) # 一条Blog对应多个
+    blogPost = models.ForeignKey('BlogPost',on_delete=models.CASCADE)
+    # 一条Blog对应多个
